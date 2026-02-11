5421d342ab6b6c99b926cb877f28b864a3990e1e
Nekoniyah <beastish.maki@gmail.com>
2026-02-11 10:04:03 +0100

    thingy

--- src/main/java/net/syndred/syndred/item/MidnightAxeItem.java
+++ src/main/java/net/syndred/syndred/item/MidnightAxeItem.java
@@ -8,10 +8,10 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 
-public class MidnighAxeItem extends AxeItem {
-	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 550, 7f, 0, 22, TagKey.create(Registries.ITEM, ResourceLocation.parse("syndred:midnigh_axe_repair_items")));
+public class MidnightAxeItem extends AxeItem {
+	private static final ToolMaterial TOOL_MATERIAL = new ToolMaterial(BlockTags.INCORRECT_FOR_IRON_TOOL, 550, 7f, 0, 22, TagKey.create(Registries.ITEM, ResourceLocation.parse("syndred:midnight_axe_repair_items")));
 
-	public MidnighAxeItem(Item.Properties properties) {
+	public MidnightAxeItem(Item.Properties properties) {
 		super(TOOL_MATERIAL, 4f, -3f, properties.fireResistant());
 	}
 }
